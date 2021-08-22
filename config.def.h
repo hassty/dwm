6b65769d1639267137abe2a6614d0a3df2300bb7
-hasty- <tikhomirovtimofei@gmail.com>
2021-08-22 23:29:26 +0300

    Remove borders from dmenu

--- config.def.h
+++ config.def.h
@@ -110,7 +110,7 @@ static const MonitorRule monrules[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-c", "-l", "10", "-h", "20", "-bw", "2", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-c", "-l", "10", "-h", "20", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 /*First arg only serves to match against key in rules*/
