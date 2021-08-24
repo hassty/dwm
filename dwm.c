0fcb8c7711dcc20b49007b3f368259f559ad60e5
-hasty- <tikhomirovtimofei@gmail.com>
2021-08-24 06:08:45 +0300

    Set systray to zero width when there are no icons

--- dwm.c
+++ dwm.c
@@ -1327,7 +1327,7 @@ getsystraywidth()
 	Client *i;
 	if(showsystray)
 		for(i = systray->icons; i; w += i->w + systrayspacing, i = i->next) ;
-	return w ? w + systrayspacing : 1;
+	return w ? w + systrayspacing : 0;
 }
 
 int
