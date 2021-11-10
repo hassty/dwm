2c0fd0f4db6d51ddcfd6c3893dbb3c7501addb0c
-hasty- <tikhomirovtimofei@gmail.com>
2021-11-10 15:27:39 +0300

    Set fonts via xresources

--- config.def.h
+++ config.def.h
@@ -16,7 +16,9 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "RobotoMono Nerd Font Mono:size=11", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char fontMain[]        = "RobotoMono Nerd Font Mono:size=10";
+static char fontEmoji[]       = "JoyPixels:pixelsize=10:antialias=true:autohint=true";
+static const char *fonts[]          = { fontMain, fontEmoji };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -121,6 +123,8 @@ static const char *vifmcmd[]       = {"f", "st", "-t", "vifm", "-e", "/home/hast
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
+        { "fontMain",           STRING,  &fontMain },
+        { "fontEmoji",          STRING,  &fontEmoji },
 		{ "normbgcolor",        STRING,  &normbgcolor },
 		{ "normbordercolor",    STRING,  &normbordercolor },
 		{ "normfgcolor",        STRING,  &normfgcolor },
